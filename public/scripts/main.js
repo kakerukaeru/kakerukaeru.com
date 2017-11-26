16e50d75efccecdfff1e6346a0a5f3df7840d720
iwanaga_kakeru <iwanaga_kakeru@cyberagent.co.jp>
2017-11-26 17:11:48 +0900

    code format by Prettier

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,10 @@
-var myImg = document.querySelector('img');
+var myImg = document.querySelector("img");
 
-myImg.onclick = function () {
-    var mySrc = myImg.getAttribute('src');
-    if ( mySrc === 'images/niconico.png') {
-        myImg.setAttribute('src', 'images/chrome.png');
-    } else {
-        myImg.setAttribute('src', 'https://stat100.ameba.jp/kakeru/ironcat.jpg');
-    }
-}
+myImg.onclick = function() {
+  var mySrc = myImg.getAttribute("src");
+  if (mySrc === "images/niconico.png") {
+    myImg.setAttribute("src", "images/chrome.png");
+  } else {
+    myImg.setAttribute("src", "https://stat100.ameba.jp/kakeru/ironcat.jpg");
+  }
+};
